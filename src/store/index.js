c48d6f49525062d79d22efa06b49e9cfb8eba9a8
MatthewJOwens <mjowens@mail.com>
2020-04-14 16:06:20 -0600

    store index set up for cars

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,52 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { createNamespacedHelpers } from 'vuex'
+import Axios from 'axios'
 
 Vue.use(Vuex)
 
+let _api = axios.create({
+  baseURL: '//bcw-sandbox.herokuapp.com/api',
+  timeout: 10000
+})
+
 export default new Vuex.Store({
   state: {
+    cars: []
   },
   mutations: {
+    setCars(state, cars) {
+      state.cars = cars
+    }
   },
   actions: {
+    async getMatchedCSSRules({ commit, dispatch }) {
+      try {
+        let res = await _api.get('cars')
+        console.log(res.data.data);
+        commit('setCars', res.data.data)
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async deleteCar({ commit, dispatch }, carId) {
+      try {
+        await _api.delete('cars/' + carId)
+        dispatch('getCars')
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async createCar({ commit, dispatch }, newCar) {
+      try {
+        let res = await _api.post('cars', newCar)
+        dispatch('getCars')
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    getHouses({ commit, dispatch }) {
+
+    }
   },
   modules: {
   }
